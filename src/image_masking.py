477beda83752c10993f3def9af74276fe2ba9625
Anna <agna@itu.dk>
2023-05-10 13:52:22 +0200

    Created a consistent formula for saving images

--- src/image_masking.py
+++ src/image_masking.py
@@ -1,20 +1,32 @@
 
-from skimage import io
+from skimage import io, img_as_ubyte
+import os
 import numpy as np
 import cv2
-from skimage import morphology
-
-#put the image and mask path
-image_path = "C:/Users/annam/Desktop/Globules/Resized/images.jpg"
-mask_path = "C:/Users/annam/Desktop/Globules/Segmentation/mask_images.jpg"
-
-#REad the image
-image = io.imread(image_path)
-mask = io.imread(mask_path)
-
-expanded_mask = mask[:, :, np.newaxis] #makes the mask 3D
-masked_image = cv2.bitwise_and(image, image, mask=expanded_mask)
 
 
+
+def mask_overlay(image_folder_path, mask_folder_path, folder_path_out):
+
+    #Iterate through all the jpg and png files in the selected folder
+    for filename in [f for f in os.listdir(image_folder_path) if f.endswith('.jpg') or f.endswith('.png')]:
+        
+        #Read in the image and the mask
+        image_path = image_folder_path + "/" + filename
+        mask_path = mask_folder_path + "/" + filename
+        image = io.imread(image_path)
+        mask = io.imread(mask_path)
+
+        # Combine the image and the mask
+        expanded_mask = mask[:, :, np.newaxis] #makes the mask 3D
+        masked_image = cv2.bitwise_and(image, image, mask=expanded_mask)
+
+        #Save the masked image in the new folder
+        new_path = folder_path_out + "/" + filename
+        io.imsave(new_path, img_as_ubyte(masked_image))
+
+image_folder_path = "C:/Users/annam/Desktop/Globules/Resized"
+mask_folder_path = "C:/Users/annam/Desktop/Globules/Segmentation"
 folder_path_out = "C:/Users/annam/Desktop/Globules/Masked"
-io.imsave(folder_path_out, masked_image)
+
+mask_overlay(image_folder_path, mask_folder_path, folder_path_out)
--- src/segmentation_script.py
+++ src/segmentation_script.py
@@ -1,29 +1,43 @@
 
-from skimage import io
+from skimage import io, util, img_as_ubyte
 from skimage.filters import threshold_otsu
 import os
 from skimage.color import rgb2gray
-from skimage import util
+
+
 
 # Set the path to the folder containing the images
 folder_path = "C:/Users/annam/Desktop/Globules/Resized"
 folder_path_out = "C:/Users/annam/Desktop/Globules/Segmentation"
 
-# List all image files in the folder
-image_files = [f for f in os.listdir(folder_path) if f.endswith('.png') or f.endswith('.jpg')]
 
-for filename in image_files:
-    # Reads the image and removes the alpha channel if it exists
-    image = io.imread(os.path.join(folder_path, filename))
-    # Ignore the alpha channel (e.g. transparency )
-    if image.shape[-1] == 4:
-        image = image[..., :3]
 
-    # Create a binary mask
-    grayscale = rgb2gray(image)
-    thresh = threshold_otsu(grayscale)
-    binary = grayscale > thresh
-    inverted_img = util.invert(binary)
-    
-    # Save the images
-    io.imsave(os.path.join(folder_path_out, 'mask_' + filename), inverted_img)
+def image_segment(folder_path_in, folder_path_out):
+
+    #Iterate through all the jpg and png files in the selected folder
+    for filename in [f for f in os.listdir(folder_path_in) if f.endswith('.jpg') or f.endswith('.png')]:
+        
+        #Read in the image
+        image_path = folder_path_in + "/" + filename
+        original = io.imread(image_path)
+
+        # Ignore the alpha channel (e.g. transparency )
+        if original.shape[-1] == 4:
+            original = original[..., :3]
+
+        # Create a binary mask
+        grayscale = rgb2gray(original)
+        thresh = threshold_otsu(grayscale)
+        binary = grayscale > thresh
+        inverted_img = util.invert(binary)
+        
+        #Save the image in the new folder
+        new_path = folder_path_out + "/" + filename
+        io.imsave(new_path, img_as_ubyte(inverted_img))
+
+
+#Specify where to pull images from and where to save them
+folder_path_in = "C:/Users/annam/Desktop/Globules/Resized"
+folder_path_out = "C:/Users/annam/Desktop/Globules/Segmentation"
+
+image_segment(folder_path_in, folder_path_out)
