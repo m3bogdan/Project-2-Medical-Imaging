3c4abdd2e6760f90b01c2d32d6a67e592963f8e6
Kayis <73404385+KayisWasTaken@users.noreply.github.com>
2023-06-01 08:26:38 +0200

    Update 03_evaluate_classifier.py

--- MAIN_FILES/03_evaluate_classifier.py
+++ MAIN_FILES/03_evaluate_classifier.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, f1_score
 from sklearn.model_selection import cross_val_score
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import pickle
 import numpy as np
-from 01_process_imgages import extract_features
+from 01_process_images import extract_features
 
 def load_model(model_file):
     with open(model_file, 'rb') as file:
@@ -13,30 +13,36 @@ def load_model(model_file):
     return models
 
 def evaluate_classifier(models, X_test, y_test):
+    model_scores = {}
     for name, model in models.items():
         y_pred = model.predict(X_test)
+        f1 = f1_score(y_test, y_pred, average='weighted')
+        model_scores[name] = f1
         print(f"Model: {name}")
         print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+        print(f"F1 Score: {f1}")
         print("Classification Report:")
         print(classification_report(y_test, y_pred))
         print("Confusion Matrix:")
         print(confusion_matrix(y_test, y_pred))
         print("-----------------------------------\n")
+    return model_scores
 
 def cross_validation_evaluation(models, X, y):
     for name, model in models.items():
         scores = cross_val_score(model, X, y, cv=5)
         print(f"Accuracy of {name}: {scores.mean():.2f} (+/- {scores.std() * 2:.2f})")
 
-def predict_from_image(models, image_path):
+def extract_features(image_path):
+    # Your feature extraction process goes here
+    pass
+
+def predict_from_image(models, image_path, best_model_name):
     features = extract_features(image_path)
     scaler = StandardScaler()
     features = scaler.fit_transform(features)
 
-    # Select the best model based on accuracy on the test set
-    best_model_name = max(models, key=lambda x: models[x].score(features, y_test))
     best_model = models[best_model_name]
-
     prediction = best_model.predict(features)
     return prediction
 
@@ -48,10 +54,14 @@ def main():
     X_test = X.sample(5)  # assuming we evaluate on a small sample
     y_test = y[X_test.index]
 
-    evaluate_classifier(models, X_test, y_test)
+    model_scores = evaluate_classifier(models, X_test, y_test)
     cross_validation_evaluation(models, X, y)
+
+    best_model_name = max(model_scores, key=model_scores.get)
+    print(f"Best model based on F1 Score is {best_model_name}")
+
     image_path = "test_image.jpg"  # replace with your image path
-    print("Prediction from image:", predict_from_image(models, image_path))
+    print("Prediction from image:", predict_from_image(models, image_path, best_model_name))
 
 if __name__ == '__main__':
     main()
