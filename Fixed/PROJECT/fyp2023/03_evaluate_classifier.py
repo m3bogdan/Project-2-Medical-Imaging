c444703eb0e6499af5471d89f6638b4c5df891e6
Alexis <serr@itu.dk>
2023-06-11 14:07:41 +0200

    Removed the return

--- Fixed/PROJECT/fyp2023/03_evaluate_classifier.py
+++ Fixed/PROJECT/fyp2023/03_evaluate_classifier.py
@@ -47,8 +47,6 @@ def classify(normal_image, segmentation_image):
     print('predicted label is', pred_label)
     print('predicted probability for the predicted class is', predicted_class_prob)
 
-    return pred_label, predicted_class_prob
-
 # Your image paths
 normal_image_path = r"C:\Users\serru\Downloads\img\Binary_mask\Test\PAT_26_37_865.png"
 segmentation_image_path = r"C:\Users\serru\Downloads\img\Test_resized\PAT_26_37_865.png"
