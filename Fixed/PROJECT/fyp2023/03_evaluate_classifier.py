534a698a7a591e8cf1a51cd958dfdfd073042a33
Alexis <serr@itu.dk>
2023-06-11 14:05:14 +0200

    Real

--- Fixed/PROJECT/fyp2023/03_evaluate_classifier.py
+++ Fixed/PROJECT/fyp2023/03_evaluate_classifier.py
@@ -2,22 +2,15 @@ import pickle
 import numpy as np
 from extract_features import extract_features
 from PIL import Image
-from extract_features import extract_features #our feature extraction
-import numpy as np
+import matplotlib.pyplot as plt
 
+def classify(normal_image, segmentation_image):
+    # Make sure the images are the same size
+    assert normal_image.shape == segmentation_image.shape, "Images are not the same size"
 
-
-# The function that should classify new images. 
-# The image and mask are the same size, and are already loaded using plt.imread
-def classify(normal_image_path, segmentation_image_path):
-        
-    # Open the normal image and the segmentation image
-    normal_image = Image.open(normal_image_path).convert("RGBA")
-    segmentation_image = Image.open(segmentation_image_path).convert("RGBA")
-
-    # Resize the images to 256x256 pixels
-    normal_image = normal_image.resize((256, 256))
-    segmentation_image = segmentation_image.resize((256, 256))
+    # Convert numpy array image to PIL image
+    normal_image = Image.fromarray((normal_image * 255).astype(np.uint8)).convert("RGBA")
+    segmentation_image = Image.fromarray((segmentation_image * 255).astype(np.uint8)).convert("RGBA")
 
     # Invert the segmentation image
     inverted_segmentation = Image.eval(segmentation_image, lambda x: 255 - x)
@@ -26,19 +19,22 @@ def classify(normal_image_path, segmentation_image_path):
     mask = inverted_segmentation.split()[0].point(lambda x: 255 if x == 0 else 0).convert("L")
 
     # Apply the mask to the normal image
-    combined_image = normal_image * binary_mask[..., None]
+    normal_image.putalpha(mask)
 
     # Create a black background
     background = Image.new("RGBA", normal_image.size, (0, 0, 0, 255))
+
     # Composite the normal image with the black background
     combined_image = Image.alpha_composite(background, normal_image)
 
+    # Convert the PIL Image object back to numpy array and convert to RGB if needed
+    combined_image = np.array(combined_image.convert("RGB"))
 
-     #Extract features (the same ones that you used for training)
+    # Extract features (the same ones that you used for training)
     X = list(extract_features(combined_image).values())
     X = np.array(X).reshape(1, -1)   
-     
-     # Load the trained classifier
+
+    # Load the trained classifier
     classifier = pickle.load(open('group02_classifier.sav', 'rb'))
 
     # Use it on this example to predict the label AND posterior probability
@@ -53,14 +49,13 @@ def classify(normal_image_path, segmentation_image_path):
 
     return pred_label, predicted_class_prob
 
-normal_image_path =""
-segmentation_image_path =""
-test_path = "C:/Users/annam/Desktop/Binary masks/Resized"
-for filename in os.listdir(test_path):
-    if filename.endswith(('.jpg', '.png')):
-        image_path = os.path.join(test_path, filename)
-        image = io.imread(image_path)
+# Your image paths
+normal_image_path = r"C:\Users\serru\Downloads\img\Binary_mask\Test\PAT_26_37_865.png"
+segmentation_image_path = r"C:\Users\serru\Downloads\img\Test_resized\PAT_26_37_865.png"
 
-        # Ignore the alpha channel (e.g. transparency)
-        if image.shape[-1] == 4:
-            image = image[..., :3]
+# Load the images
+normal_image = plt.imread(normal_image_path)
+segmentation_image = plt.imread(segmentation_image_path)
+
+# Use the function
+classify(normal_image, segmentation_image)
